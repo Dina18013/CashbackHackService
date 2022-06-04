87240a00c75f359048ebe041fe10864cfa62037a
Надежда Волынец <vnv18013@gmail.com>
2022-06-04 23:46:30 +0300

    fix bug

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -48,7 +48,7 @@ public class CashbackHackServiceTest {
     public void ShouldReturn0Amount1000() {
 
         int actual = service.remain(1000);
-        int expected = 0;
+        int expected = 1000;
 
         assertEquals(actual, expected);
     }
