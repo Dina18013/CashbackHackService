e5080f87e4ec6e0b55e01c772c191e76043cf5b3
Надежда Волынец <vnv18013@gmail.com>
2022-06-04 23:39:51 +0300

    new tests testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,53 +9,47 @@ public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
-    void ShouldReturn2000Amount2000() {
-
-        int actual = service.remain(2000);
-        int expected = 1000;
-
-        assertEquals(actual, expected);
-
-    }
-
-    @Test
-    void ShouldReturn900Amount100() {
-
-        int actual = service.remain(100);
-        int expected = 900;
-
-        assertEquals(actual, expected);
-
-    }
-
-
-    @Test
-    void ShouldReturn500Amount500() {
+    public void ShouldReturn500Amount500() {
 
         int actual = service.remain(500);
         int expected = 500;
 
         assertEquals(actual, expected);
-
     }
 
     @Test
-    void ShouldReturn1000Amount1000() {
+    public void ShouldReturn100Amount900() {
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ShouldReturn800Amount1200() {
+
+        int actual = service.remain(1200);
+        int expected = 800;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ShouldReturn300Amount1700() {
+
+        int actual = service.remain(1700);
+        int expected = 300;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ShouldReturn0Amount1000() {
 
         int actual = service.remain(1000);
-        int expected = 1000;
+        int expected = 0;
 
         assertEquals(actual, expected);
-
-    }
-
-    @Test
-    void ShouldReturn950Amount1050() {
-
-        int actual = service.remain(1050);
-        int expected = 950;
-
-        assertEquals(actual, expected);
-
     }
 }
